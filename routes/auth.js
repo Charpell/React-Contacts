9e9285eab268402c5461f6505060eb582e859989
charpell <charpellumeh@gmail.com>
2019-08-17 14:50:57 +0100

    middleware

--- routes/auth.js
+++ routes/auth.js
@@ -6,10 +6,17 @@ const jwt = require('jsonwebtoken')
 const config = require('config')
 
 const User = require('../models/User')
+const auth = require('../middleware/auth')
 
 
-router.get('/', (req, res) => {
-  res.send('Get logged in user')
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    res.json(user)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
 })
 
 router.post('/', 
