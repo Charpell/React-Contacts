797ac5c287be90b1aab1fc3dc25c8a6528ea2ffd
charpell <charpellumeh@gmail.com>
2019-08-23 13:47:17 +0100

    register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,22 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import AlertContext from '../../contex/alert/alertContext'
+import AuthContext from '../../contex/auth/authContext'
 
 
 export default function Register() {
   const alertContext = useContext(AlertContext)
+  const authContext = useContext(AuthContext)
   
   const { setAlert } = alertContext;
+
+  const { register, error, clearErrors } = authContext
+
+  useEffect(() => {
+    if(error === 'User already exists') {
+      setAlert(error, 'danger');
+      clearErrors()
+    }
+  }, [error])
 
   const [user, setUser] = useState({
     name: '',
@@ -25,7 +36,11 @@ export default function Register() {
     } else if (password !== password2) {
       setAlert('Passwords do not match', 'danger')
     }
-    console.log('Register submit')
+    register({
+      name,
+      email,
+      password
+    })
   }
 
   return (
