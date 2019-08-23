4eca54b1ab9fd23833864015cc1f8a5bc4e733c2
charpell <charpellumeh@gmail.com>
2019-08-23 22:08:43 +0100

    login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
+import AlertContext from '../../contex/alert/alertContext'
+import AuthContext from '../../contex/auth/authContext'
 
-export default function Login() {
+
+export default function Login(props) {
+  const alertContext = useContext(AlertContext)
+  const authContext = useContext(AuthContext)
+
+  const { setAlert } = alertContext;
+
+  const { login, error, clearErrors, isAuthenticated } = authContext
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/')
+    }
+    if(error === 'Invalid Credentials') {
+      setAlert(error, 'danger');
+      clearErrors()
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history])
+
   const [user, setUser] = useState({
     email: '',
     password: ''
@@ -12,7 +33,14 @@ export default function Login() {
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log('Login submit')
+    if(email === '' || password === '') {
+      setAlert('Please enter all fields', 'danger')
+    } else {
+    login({
+      email,
+      password
+    })
+  }
   }
 
   return (
@@ -23,11 +51,11 @@ export default function Login() {
       <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="email">Email Address</label>
-          <input type="email" name="email" value={email} onChange={onChange} />
+          <input type="email" name="email" value={email} onChange={onChange} required />
         </div>
         <div className="form-group">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" value={password} onChange={onChange} />
+          <input type="password" name="password" value={password} onChange={onChange} required />
         </div>
         <input type="submit" value="Login" className="btn btn-primary btn-block" />
       </form>
--- client/src/contex/auth/AuthState.js
+++ client/src/contex/auth/AuthState.js
@@ -66,7 +66,28 @@ const AuthState = props => {
   }
 
   // Login User
-  const login = () => console.log('loadUser')
+  const login = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.post('/api/auth', formData, config)
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      })
+      loadUser()
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.message
+      })
+    }
+  }
 
   // Logout
   const logout = () => console.log('loadUser')
